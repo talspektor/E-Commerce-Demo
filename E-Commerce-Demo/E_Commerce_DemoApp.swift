84370e11369436aeb5300eddc8de9f74a2c9a8b2
Tal Spektor <talspektor@gmail.com>
2022-04-09 18:17:06 +0300

    Initial Commit

---
+++ E-Commerce-Demo/E_Commerce_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_DemoApp.swift
+//  E-Commerce-Demo
+//
+//  Created by Tal talspektor on 09/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
